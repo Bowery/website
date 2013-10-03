a8c5f33f9a08ef751ad9f11fde5b4cb9d5b1d887
Steve Kaliski <sjkaliski@gmail.com>
2013-10-03 15:13:37 -0400

    fix

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var http = require('http')
 var path = require('path')
+var request = require('request')
 
 var app = express()
 
@@ -17,15 +18,37 @@ app.use(app.router)
 app.use(require('less-middleware')({ src: __dirname + '/public' }))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// TODO(steve): Replace this with propellant.
+var API_ENDPOINT = 'http://api.bowery.io'
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler())
+  API_ENDPOINT = 'http://bowery2.apiary.io'
 }
 
 ['/', '/docs', '/pricing', '/signup'].forEach(function (v) {
   app.get(v, function (req, res) {
     res.render(v == '/' ? 'index' : v.slice(1))
   })
+})
+
+app.post('/developers', function (req, res) {})
+
+app.put('/developers/:token', function (req, res) {})
+
+app.get('/me/:token', function (req, res) {
+  res.end('token')
+})
+
+app.post('/organizations/:name/exists', function (req, res) {})
+
+app.put('/organizations/:id', function (req, res) {})
+
+app.post('/organizations/developer', function (req, res) {})
+
+app.get('/organizations/invite/:token', function (req, res) {
+  res.end('token')
 })
 
 app.listen(app.get('port'), function(){
